58d843a3bd1d5fa30e0e84c5eb5a18f9662322d3
kentobia132 <thobhi2@gmail.com>
2024-03-10 14:56:13 +0100

    Plus and Minus button, Playlist name state functionalities added

--- src/App.js
+++ src/App.js
@@ -20,23 +20,44 @@ function App() {
     { name: "we move", artist: "jonneydrill", album: "My Life", id: "3" },
     { name: "Fatherly Love", artist: "Tekno", album: "Rebel Heart", id: "4" },
   ]);
-  const [playListName, setPlayListName] = useState('Example playlistname')
-  const [playListTrack, setPlayListTrack] = useState([ {
+  const [playListName, setPlayListName] = useState("Example playlistname");
+  const [playListTrack, setPlayListTrack] = useState([
+    {
       name: "Example1",
       artist: "DavidOOfficial",
       album: "Timeless",
-      id: "1",
+      id: "5",
     },
     {
       name: "Example2",
       artist: "The Police",
       album: "Ghost in the Machine",
-      id: "2",
+      id: "6",
     },
-    { name: "Example3", artist: "jonneydrill", album: "My Life", id: "3" },
-    { name: "Example4", artist: "jonneydrill", album: "My Life", id: "3" },
-    { name: "Example5", artist: "jonneydrill", album: "My Life", id: "3" },
-    { name: "Example6", artist: "Tekno", album: "Rebel Heart", id: "4" },])
+    { name: "Example3", artist: "jonneydrill", album: "My Life", id: "7" },
+    { name: "Example4", artist: "jonneydrill", album: "My Life", id: "8" },
+    { name: "Example5", artist: "jonneydrill", album: "My Life", id: "9" },
+    { name: "Example6", artist: "Tekno", album: "Rebel Heart", id: "10" },
+  ]);
+
+  const updatePlaylistName = (name) => {
+    setPlayListName(name);
+  };
+
+  const addTrack = (track) => {
+    const existingTrack = playListTrack.find(({ id }) => id === track.id);
+    if (!existingTrack) {
+      setPlayListTrack([...playListTrack, track]);
+      console.log(playListName);
+    } else {
+      console.log(`${track.name} from ${track.artist} already exists`);
+    }
+  };
+
+  const removeTrack = (track) => {
+    setPlayListTrack(playListTrack.filter(({ id }) => id !== track.id));
+  };
+
   return (
     <div className=" bg-[url('./img/background_photo.jpg')] bg-cover">
       <nav className="text-center bg-blue-950 text-white text-2xl py-3">
@@ -45,8 +66,12 @@ function App() {
       <div className="flex justify-center flex-col items-center mt-6">
         <SearchBar />
         <div className="flex flex-row my-12 w-9/12 mx-auto">
-          <SearchResults userSearchResult={searchData} />
-          <Playlist playListName={playListName} playListTrack={playListTrack} />
+          <SearchResults userSearchResult={searchData} onAdd={addTrack} />
+          <Playlist
+            onNameChange={updatePlaylistName}
+            playListTrack={playListTrack}
+            onRemove={removeTrack}
+          />
         </div>
       </div>
     </div>
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,18 +1,30 @@
-import React from 'react'
-import Tracklist from './Tracklist'
-import Button from './Button'
+import React from "react";
+import Tracklist from "./Tracklist";
+import Button from "./Button";
 
-function Playlist({playListName, playListTrack}) {
+function Playlist({ onNameChange, playListTrack, onRemove }) {
+  function handleNameChange({ target }) {
+    onNameChange(target.value);
+  }
+
   return (
-<div className=' bg-blue-950 px-3 py-3 w-1/2 mx-8 '>
-      <h1 className='text-white font-extrabold'> Playlist </h1>
-      <input defaultValue={'New Playlist'} />
-      <Tracklist userSearchResult={playListTrack} isRemoval={true} />
-      <div className='py-6 flex justify-center'>
-        
-      <Button text={'Add to Spotify'} />
+    <div className=" bg-blue-950 px-3 py-3 w-1/2 mx-8 ">
+      <h1 className="text-white font-extrabold"> Playlist </h1>
+      <input
+        className="bg-transparent border-b-[1px] py-2 w-[80%] outline-none text-white mb-5 "
+        defaultValue={"New Playlist"}
+        onChange={handleNameChange}
+      />
+      <Tracklist
+        userSearchResult={playListTrack}
+        isRemoval={true}
+        onRemove={onRemove}
+      />
+      <div className="py-6 flex justify-center">
+        <Button text={"Add to Spotify"} />
       </div>
-      </div>  )
+    </div>
+  );
 }
 
-export default Playlist
+export default Playlist;
